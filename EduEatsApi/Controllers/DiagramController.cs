3fabcbba8b9991dd6b4c1b5c488d98869cfe5637
Orex-x <vipboy78038@gmail.com>
2023-06-01 12:52:52 +0300

    add product diagram

---
+++ EduEatsApi/Controllers/DiagramController.cs
@@ -0,0 +1,43 @@
+﻿using EduEatsApi.Services;
+using EduEatsApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduEatsApi.Controllers;
+
+public class DiagramController : Controller
+{
+    private readonly DatabaseContext _context;
+
+    public DiagramController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> ProductDiagram()
+    {
+        var labels = await _context.Products.Select(x => x.Name).ToListAsync();
+        
+        var records = await _context.Orders
+            .Include(x => x.Products)
+            .ThenInclude(x => x.Product)
+            .ToListAsync();
+
+
+        var data = new List<int>();
+        
+        labels.ForEach(label =>
+        {
+            var count = records.Sum(x => x.Products.Where(y
+                => y.Product.Name == label).Sum(z => z.Amount));
+        });
+        
+        var model = new ProductDiagramViewModel
+        {
+            BarLabels = labels,
+            BarData = data
+        };
+
+        return View(model);
+    }
+}
---
+++ EduEatsApi/ViewModels/ProductDiagramViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EduEatsApi.ViewModels;
+
+public class ProductDiagramViewModel
+{
+    public List<string> BarLabels { get; set; }
+    public List<int> BarData { get; set; }
+}
